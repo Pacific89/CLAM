1509050a38e16819d3e507c26bb02ee570cf972b
philipp faulhammer <philipp.faulhammer@gmail.com>
2021-10-04 19:57:24 +0200

    add config variables

--- start_create_patches.py
+++ start_create_patches.py
@@ -7,12 +7,12 @@ with open("usr/local/config/clam_command_config.json") as json_file:
     clam_config = json.loads(json_file.read())
 
 patch_size = 128 # set patch size (128 needed for ARA-NET / 224 needed for VGG16 feature extraction)
-seg = "--seg"
-patch = "--patch"
-stitch = "--stitch"
-no_auto_skip = "--no_auto_skip"
+seg = "--seg" if json.loads(clam_config["seg"].lower()) else ""
+patch = "--patch" if json.loads(clam_config["patch"].lower()) else ""
+stitch = "--stitch" if json.loads(clam_config["stitch"].lower()) else ""
+no_auto_skip = "--no_auto_skip" if json.loads(clam_config["not_auto_skip"].lower()) else ""
 preset = "--preset preset.csv"
-patch_level = "--patch_level 8" # downsample level for patch calculation
+patch_level = "--patch_level {0}".format(int(clam_config["patch_level"])) # downsample level for patch calculation
 process_list = "--process_list process_list.csv"
 output_path = clam_config["output_path"] # set output folder
 
