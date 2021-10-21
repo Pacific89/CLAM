464c38d914bae57599023525482d59021767ed8e
Pacific89 <philipp.faulhammer@medunigraz.at>
2021-10-21 09:56:08 +0200

    check file and adjust output path

--- start_create_patches.py
+++ start_create_patches.py
@@ -17,27 +17,33 @@ def call_create_patches(args):
         except:
             print("Not a valid json config string. Using default")
 
-    patch_size = clam_config["patch_size"] # set patch size (128 needed for ARA-NET / 224 needed for VGG16 feature extraction)
-    seg = "--seg" if json.loads(clam_config["seg"].lower()) else ""
-    patch = "--patch" if json.loads(clam_config["patch"].lower()) else ""
-    stitch = "--stitch" if json.loads(clam_config["stitch"].lower()) else ""
-    no_auto_skip = "--no_auto_skip" if json.loads(clam_config["no_auto_skip"].lower()) else ""
-    preset = "--preset preset.csv"
-    patch_level = "--patch_level {0}".format(int(clam_config["patch_level"])) # downsample level for patch calculation
-    process_list = "--process_list process_list.csv"
-    output_path = clam_config["output_path"] # set output folder
-
-    print("CONFIG:")
-    print(clam_config)
-    # get filename from command line arguments:
     file_name = sys.argv[1]
-    # create input path:
-    input_path = "/usr/local/data/{0}".format(file_name)
-    # create correct command to create patch coordinates using CLAM:
-    clam_command = "python usr/local/src/clam/create_patches_fp.py --source {0} --save_dir {1} --patch_size {2} {3} {4} {5}".format(input_path, output_path, patch_size, seg, patch, stitch)
-    # start CLAM:
-    os.system(clam_command)
-    print(clam_command)
+
+    if file_name.endswith(".svs"):
+
+        patch_size = clam_config["patch_size"] # set patch size (128 needed for ARA-NET / 224 needed for VGG16 feature extraction)
+        seg = "--seg" if json.loads(clam_config["seg"].lower()) else ""
+        patch = "--patch" if json.loads(clam_config["patch"].lower()) else ""
+        stitch = "--stitch" if json.loads(clam_config["stitch"].lower()) else ""
+        no_auto_skip = "--no_auto_skip" if json.loads(clam_config["no_auto_skip"].lower()) else ""
+        preset = "--preset preset.csv"
+        patch_level = "--patch_level {0}".format(int(clam_config["patch_level"])) # downsample level for patch calculation
+        process_list = "--process_list process_list.csv"
+        output_path = clam_config["output_path"] + "/{0}/clam".format(filename.split(".svs")[0]) # set output folder
+
+        print("CONFIG:")
+        print(clam_config)
+        # get filename from command line arguments:
+        # create input path:
+        input_path = "/usr/local/data/{0}".format(file_name)
+        # create correct command to create patch coordinates using CLAM:
+        clam_command = "python usr/local/src/clam/create_patches_fp.py --source {0} --save_dir {1} --patch_size {2} {3} {4} {5}".format(input_path, output_path, patch_size, seg, patch, stitch)
+        # start CLAM:
+        os.system(clam_command)
+        print(clam_command)
+
+    else:
+        print("Wrong filename or type: ", file_name)
 
 def call_extract_features(args):
     print("Calling extract features...")
