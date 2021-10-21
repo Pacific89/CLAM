87e92ac41ddacd7082d911718beb111f1ac847c4
Pacific89 <philipp.faulhammer@medunigraz.at>
2021-10-21 12:11:37 +0200

    input path

--- start_create_patches.py
+++ start_create_patches.py
@@ -50,7 +50,7 @@ def call_extract_features(args):
     csv_path = output_path + "/process_list_autogen.csv"
     data_h5_dir = output_path + "/patches"
 
-    clam_command = "python usr/local/src/clam/extract_features_fp.py --data_slide_dir {0} --csv_path {1} --feat_dir {2} --data_h5_dir {3}".format(input_folder, csv_path, feat_dir, data_h5_dir)
+    clam_command = "python usr/local/src/clam/extract_features_fp.py --data_slide_dir {0} --csv_path {1} --feat_dir {2} --data_h5_dir {3}".format(input_path, csv_path, feat_dir, data_h5_dir)
 
     print("Calling extract features...")
     print(clam_command)
